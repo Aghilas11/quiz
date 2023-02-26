1831165bb7bf47d1ba15f497a4368824e2f25e0b
AGHILAS <halouane.aghilas@yahoo.com>
2023-02-27 00:30:28 +0100

    le quiz des capitales

---
+++ styles/index.js
@@ -0,0 +1,143 @@
+const btn1 = document.getElementById("btn-1");
+const btn2 = document.getElementById("btn-2");
+const btn3 = document.getElementById("btn-3");
+const btn4 = document.getElementById("btn-4");
+const btn5 = document.getElementById("btn-5");
+const btn6 = document.getElementById("btn-6");
+const btn7 = document.getElementById("btn-7");
+const btn8 = document.getElementById("btn-8");
+const btn9 = document.getElementById("btn-9");
+const btn10 = document.getElementById("btn-10");
+const btn11 = document.getElementById("btn-11");
+const btn12 = document.getElementById("btn-12");
+const btn13 = document.getElementById("btn-13");
+const btn14 = document.getElementById("btn-14");
+const btn15 = document.getElementById("btn-15");
+const btn16 = document.getElementById("btn-16");
+const btn17 = document.getElementById("btn-17");
+const btn18 = document.getElementById("btn-18");
+const btn19 = document.getElementById("btn-19");
+const btn20 = document.getElementById("btn-20");
+alert("Si tu n'as pas la note compléte vas retourner au primaire !");
+
+btn1.addEventListener("click", () => {
+  btn1.style.background = "green";
+  btn2.style.background = "red";
+  btn3.style.background = "red";
+  btn4.style.background = "red";
+});
+btn2.addEventListener("click", () => {
+  btn1.style.background = "green";
+  btn2.style.background = "red";
+  btn3.style.background = "red";
+  btn4.style.background = "red";
+});
+btn3.addEventListener("click", () => {
+  btn1.style.background = "green";
+  btn2.style.background = "red";
+  btn3.style.background = "red";
+  btn4.style.background = "red";
+});
+btn4.addEventListener("click", () => {
+  btn1.style.background = "green";
+  btn2.style.background = "red";
+  btn3.style.background = "red";
+  btn4.style.background = "red";
+});
+
+btn5.addEventListener("click", () => {
+  btn5.style.background = "red";
+  btn6.style.background = "red";
+  btn7.style.background = "red";
+  btn8.style.background = "green";
+});
+btn6.addEventListener("click", () => {
+  btn5.style.background = "red";
+  btn6.style.background = "red";
+  btn7.style.background = "red";
+  btn8.style.background = "green";
+});
+btn7.addEventListener("click", () => {
+  btn5.style.background = "red";
+  btn6.style.background = "red";
+  btn7.style.background = "red";
+  btn8.style.background = "green";
+});
+btn8.addEventListener("click", () => {
+  btn5.style.background = "red";
+  btn6.style.background = "red";
+  btn7.style.background = "red";
+  btn8.style.background = "green";
+});
+btn9.addEventListener("click", () => {
+  btn9.style.background = "green";
+  btn10.style.background = "red";
+  btn11.style.background = "red";
+  btn12.style.background = "red";
+});
+btn10.addEventListener("click", () => {
+  btn9.style.background = "green";
+  btn10.style.background = "red";
+  btn11.style.background = "red";
+  btn12.style.background = "red";
+});
+btn11.addEventListener("click", () => {
+  btn9.style.background = "green";
+  btn10.style.background = "red";
+  btn11.style.background = "red";
+  btn12.style.background = "red";
+});
+btn12.addEventListener("click", () => {
+  btn9.style.background = "green";
+  btn10.style.background = "red";
+  btn11.style.background = "red";
+  btn12.style.background = "red";
+});
+btn13.addEventListener("click", () => {
+  btn13.style.background = "red";
+  btn14.style.background = "red";
+  btn15.style.background = "green";
+  btn16.style.background = "red";
+});
+btn14.addEventListener("click", () => {
+  btn13.style.background = "red";
+  btn14.style.background = "red";
+  btn15.style.background = "green";
+  btn16.style.background = "red";
+});
+btn15.addEventListener("click", () => {
+  btn13.style.background = "red";
+  btn14.style.background = "red";
+  btn15.style.background = "green";
+  btn16.style.background = "red";
+});
+btn16.addEventListener("click", () => {
+  btn13.style.background = "red";
+  btn14.style.background = "red";
+  btn15.style.background = "green";
+  btn16.style.background = "red";
+});
+btn17.addEventListener("click", () => {
+  btn17.style.background = "green";
+  btn18.style.background = "red";
+  btn19.style.background = "red";
+  btn20.style.background = "red";
+});
+btn18.addEventListener("click", () => {
+  btn17.style.background = "green";
+  btn18.style.background = "red";
+  btn19.style.background = "red";
+  btn20.style.background = "red";
+});
+btn19.addEventListener("click", () => {
+  btn17.style.background = "green";
+  btn18.style.background = "red";
+  btn19.style.background = "red";
+  btn20.style.background = "red";
+});
+btn20.addEventListener("click", () => {
+  btn17.style.background = "green";
+  btn18.style.background = "red";
+  btn19.style.background = "red";
+  btn20.style.background = "red";
+});
